bf3e78a17c750e2ebbb1b880f58c275b033ff0fd
popawaw <gkwhddbs77@knou.ac.kr>
2021-05-14 17:14:51 +0900

    Created code pipe validator

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Delete,
+  Body,
+  Patch,
+} from '@nestjs/common';
+import { Movie } from './entities/movie.entitiy';
+import { MoviesService } from './movies.service';
+import { CreateMovieDto } from './dto/create-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') id: number): Movie {
+    return this.moviesService.getOne(id);
+  }
+
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete('/:id')
+  delete(@Param('id') movieId: number) {
+    return this.moviesService.deleteOne(movieId);
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: number, @Body() updateData: any) {
+    return this.moviesService.update(movieId, updateData);
+  }
+}
